a4e15fd674182eb9cba96fe9790401ba71f79dcc
lou8882 <nolanryanlewis@hotmail.com>
2024-10-05 17:12:52 -0700

    add back button on map gen page and remove null values when generating map to fix error

--- st_pages/gen_map.py
+++ st_pages/gen_map.py
@@ -1,38 +1,12 @@
 import streamlit as st
 from utils import constants, helpers
 from utils.session_data import SessionData
-
-
-# @st.dialog("Create the Map")
-# def create_map_form(session: SessionData) -> None:
-#     # col1, col2 = st.columns([.2, .8])
-#     lat_column = st.selectbox("Column Containing Latitude",
-#                             key=constants2.SELECTBOX_LAT_COL,
-#                             options=session.data.columns,
-#                             index=None)
-    
-#     lon_column = st.selectbox("Column Containing Longitude",
-#                             key=constants2.SELECTBOX_LON_COL,
-#                             options=session.data.columns,
-#                             index=None)
-    
-#     color_col = st.selectbox("(Optional) Column for color coding",
-#                             key=constants2.SELECTBOX_COLOR,
-#                             options=session.data.columns,
-#                             index=None)
-    
-#     if lat_column != "" and lon_column != "":
-#         if st.button("Continue",
-#                      key=constants2.BUTTON_CONTINUE,
-#                      on_click=)
     
 
 def gen_map_page():
     st.title("Create map")
     session = helpers.cur_session()
 
-
-    # col1, col2 = st.columns([.2, .8])
     lat_column = st.selectbox("Column Containing Latitude",
                             key=constants.SELECTBOX_LAT_COL,
                             options=session.data.columns,
@@ -55,6 +29,9 @@ def gen_map_page():
             df = helpers.append_rand_color(df, color_col)
         else:
             df[constants.COLOR_COLUMN_NAME] = helpers.generate_random_color()
+        
+        df = df.dropna()
+
         st.map(session.data,
                latitude=lat_column,
                longitude=lon_column,
@@ -62,3 +39,8 @@ def gen_map_page():
                )
     
 
+    st.button("Return to Data",
+              key=constants.BUTTON_BACK,
+              on_click=session.new_page,
+              args=[constants.PAGE_EDIT_DATA]
+              )
