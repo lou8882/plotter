7a6c0bd5827b7dea60a317bb7fb2ebe4cf5e9b56
lou8882 <nolanryanlewis@hotmail.com>
2024-10-06 10:59:18 -0700

    remove debug sidebar

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import streamlit as st
 from utils import constants, router
 from utils import helpers
 from utils import navigation
-# base_dir = os.path.dirname(os.path.realpath(__file__))
+import os
 
 print(f"\n============\nreload\n") 
-navigation.get_sidebar()
+if os.getenv("HOSTNAME") == "streamlit":
+    navigation.get_sidebar()
 
 session = helpers.cur_session()
 
